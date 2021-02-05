53cfdead34f6e02db53784ccae5b4436ca966c9d
thibaulltt <devillelethibault@gmail.com>
2021-02-05 18:32:46 +0100

    Fixed error messages on configure.sh
    Will not show error messages from mkdir anymore

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 # Start by greeting the user
 echo "Configuring TinyTIFF ..."
 #make lib directory :
-mkdir lib
+mkdir lib 2> /dev/null
 cd TinyTIFF/
-mkdir release
+mkdir release 2> /dev/null
 cmake -S. -Brelease -DCMAKE_BUILD_TYPE=Release -DTinyTIFF_BUILD_TESTS=OFF -DTinyTIFF_BUILD_DECORATE_LIBNAMES_WITH_BUILDTYPE=OFF -DCMAKE_INSTALL_PREFIX=/home/thibault/git/stage/TinyTIFF/../lib -DTinyTIFF_BUILD_STATIC_LIBS=ON
 cmake --build release --target install
 cd ../
