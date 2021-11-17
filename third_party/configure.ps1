858a2d84b9dfabc507d6eda20429604fc6565067
Thibault de Villèle / LIRMM <thibault.de-villele@lirmm.fr>
2021-11-17 17:06:35 +0100

    Windows compilation fix & ignore dirty modules
    ---
    
    Set the default 'ignore' state of submodules to 'dirty', in order to
    completely ignore all local changes made by the compilation process.
    Made necessary in order to 'properly' compile under Windows ... What a
    complete mess it is.
    
    Also, fixed the compilation script ONCE MORE because apparently Windows
    is _really_ picky about cross-compilation between two nearly identical
    compilers (Qt's provided MinGW and regular MinGW both at v8.1).
    
    As such, it is better to spin up a Qt-enabled terminal as administrator
    and launch the .bat script directly from the third_party/ folder if
    compilation on Windows is required.

--- third_party/configure.ps1
+++ third_party/configure.ps1
@@ -9,13 +9,14 @@
 # Executable paths :
 [string]$CMakeCmdPath = ""
 [string]$QMakeCmdPath = ""
+[string]$GitPath = ""
 [string]$GNUMakeCmdPath = ""
 [int]$IsSystemValid = 1;
 
 # The Powershell path is already in the third_party directory, no
 # need to append it once more to the library path :
 [string]$ProjectRootPath = (Split-Path -Parent $PSCommandPath)
-[string]$qglviewerPath = ($ProjectRootPath)+"\libQGLViewer\QGLViewer"
+[string]$qglviewerPath = ($ProjectRootPath)+"\libQGLViewer"
 [string]$TinyTIFFPath = ($ProjectRootPath)+"\TinyTIFF"
 [string]$libTIFFPath = ($ProjectRootPath)+"\libtiff"
 [string]$niftiPath = ($ProjectRootPath)+"\nifticlib"
@@ -84,12 +85,19 @@ function Clear-GitAll {
 
 function Start-GitCompile {
 	# Checks the right programs are available, then creates the compiled_libraries folder :
-	$Global:CMakeCmdPath = (Get-Command -Name cmake.exe).Source
+	$Global:GitPath = (Get-Command -Name git.exe).Source
+	$Global:CMakeCmdPath = "C:\Qt\Tools\CMake_64\bin\cmake.exe"
 	$Global:QMakeCmdPath = "C:\Qt\5.15.2\mingw81_64\bin\qmake.exe"
 	$Global:GNUMakeCmdPath = "C:\Qt\Tools\mingw810_64\bin\mingw32-make.exe"
 
 	if (Test-Path -Path $Global:CMakeCmdPath -PathType Leaf) {
 		Write-Host "CMake was found at : "${Global:CMakeCmdPath}
+	} else {
+		$Global:IsSystemValid = 0;
+	}
+
+	if (Test-Path -Path $Global:GitPath -PathType Leaf) {
+		Write-Host "git was found at : "${Global:GitPath}
 	} else {
 		$Global:IsSystemValid = 0;
 	}
@@ -120,7 +128,7 @@ function Publish-GitTinyTIFF {
 		Start-Process -FilePath $Global:CMakeCmdPath -NoNewWindow -Wait -ArgumentList `
 			"-S. -Brelease -G `"MinGW Makefiles`" -DCMAKE_BUILD_TYPE=Release -DTinyTIFF_BUILD_TESTS=OFF -DTinyTIFF_BUILD_DECORATE_LIBNAMES_WITH_BUILDTYPE=OFF -DCMAKE_INSTALL_PREFIX=$CompiledLibPath -DTinyTIFF_BUILD_STATIC_LIBS=ON"
 		# Call CMake to compile the project :
-		Start-Process cmake -NoNewWindow -Wait -ArgumentList "--build release --target install"
+		Start-Process -FilePath $Global:CMakeCmdPath -NoNewWindow -Wait -ArgumentList "--build release --target install"
 	}
 	# Finish the process :
 	Write-Host "Configuration of TinyTIFF done."
@@ -162,7 +170,7 @@ function Publish-GitQGLViewer {
 		$env:LIBRARY_PATH+=";$qglviewerPath;$qglviewerPath\QGLViewer;$env:PATH";
 		# Call GNU's make :
 		Start-Process -FilePath $Global:GNUMakeCmdPath -NoNewWindow -Wait -ArgumentList `
-			"--no-print-directory --silent -j release"
+			"--no-print-directory --silent -j sub-QGLViewer"
 	}
 	# Finish the process :
 	Write-Host "Configuration of QGLViewer done."
@@ -178,9 +186,12 @@ function Publish-GitNifti {
 		# Here we cast the result to void in order to have no output on the CMD/PS :
 		[void](New-Item -Force -Path $niftiPath -Name "release" -ItemType "directory")
 
+		[string]$cmakeBuildArgs="-S. -Brelease -G `"MinGW Makefiles`" -DCMAKE_BUILD_TYPE=Release -DZLIB_ROOT=`"$CompiledLibPath`" -DNIFTI_BUILD_TESTING=OFF -DNIFTI_SHELL_SCRIPT_TESTS=OFF -DNIFTI_BUILD_APPLICATIONS=OFF -DNIFTI_INSTALL_NO_DOCS=TRUE -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=$CompiledLibPath "
+
+		# Patch the znzlib cmake file to include the local/global zlib include dirs :
+		Start-Process -FilePath $Global:GitPath -NoNewWindow -Wait -ArgumentList " apply ../znz_include_zlib.patch "
 		# Start the CMake generation process :
-		Start-Process -FilePath $Global:CMakeCmdPath -NoNewWindow -Wait -ArgumentList `
-			"-S. -Brelease -G `"MinGW Makefiles`" -DCMAKE_BUILD_TYPE=Release -DZLIB-ROOT=$CompiledLibPath -DNIFTI_BUILD_TESTING=OFF -DNIFTI_SHELL_SCRIPT_TESTS=OFF -DNIFTI_BUILD_APPLICATIONS=OFF -DNIFTI_INSTALL_NO_DOCS=TRUE -DCMAKE_INSTALL_PREFIX=$CompiledLibPath "
+		Start-Process -FilePath $Global:CMakeCmdPath -NoNewWindow -Wait -ArgumentList $cmakeBuildArgs
 		# Call CMake to compile the project :
 		Start-Process -FilePath $Global:CMakeCmdPath -NoNewWindow -Wait -ArgumentList `
 			"--build release --target install"
@@ -243,7 +254,7 @@ if ($args.Count -gt 0) {
 	}
 	# And TinyTIFF :
 	if ($args.Contains("tinytiff")) {
-		Publish-GitlibTIFF
+		Publish-GitTinyTIFF
 	}
 	# And finally with QGLViewer :
 	if ($args.Contains("qglviewer")) {
