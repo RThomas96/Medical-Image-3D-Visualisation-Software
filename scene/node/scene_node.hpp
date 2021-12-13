f9a84971a2f5a4672bf63766bfd3ace006aeb955
Thibault de Villèle / LIRMM <thibault.de-villele@lirmm.fr>
2021-12-13 15:05:54 +0100

    Add stub for the scene node
    ---
    
    Simply to create the branch.

---
+++ scene/node/scene_node.hpp
@@ -0,0 +1,14 @@
+#ifndef VISUALISATION_SCENE_NODE_SCENE_NODE_HPP_
+#define VISUALISATION_SCENE_NODE_SCENE_NODE_HPP_
+
+#include <QObject>
+
+class SceneNode {
+public:
+	SceneNode();
+	~SceneNode();
+protected:
+	//
+};
+
+#endif // VISUALISATION_SCENE_NODE_SCENE_NODE_HPP_
