78532163954ca616c6dbf92a9b140b29c3d335f1
RThomas96 <thomas.richard10@lilo.org>
2022-10-04 10:05:40 +0200

    Add file drawable grid

---
+++ grid/drawable/drawable_grid.cpp
@@ -0,0 +1,20 @@
+#include "drawable_grid.hpp"
+
+#include <glm/gtc/type_ptr.hpp>
+
+#include <memory>
+
+void DrawableGrid::initializeGL(ShaderCompiler::GLFunctions *functions) {
+    this->gl = functions;
+    // Create the right shader program :
+    auto compiler = std::make_unique<ShaderCompiler>(functions);
+    compiler->vertexShader_file("../shaders/base_mesh.vert").fragmentShader_file("../shaders/base_mesh.frag");
+    if (compiler->compileShaders()) {
+        this->program = compiler->programName();
+    } else {
+        std::cerr << "Error while building shaders for drawable mesh.\n" << compiler->errorString() << "\n";
+    }
+
+    this->color = glm::vec4(1., 0., 0., 1.);
+    this->lightPosition = glm::vec3(500., 500., 500.);
+}
---
+++ grid/drawable/drawable_grid.hpp
@@ -0,0 +1,50 @@
+#ifndef VISUALISATION_MESHES_DRAWABLE_SURFACE_MESH_HPP_
+#define VISUALISATION_MESHES_DRAWABLE_SURFACE_MESH_HPP_
+
+//#include "../geometry/surface_mesh.hpp"
+
+#include "../../legacy/meshes/drawable/shaders.hpp"
+#include "qobjectdefs.h"
+
+#include <QOpenGLContext>
+#include <QOpenGLExtraFunctions>
+#include <QOpenGLFunctions>
+#include <QOpenGLFunctions_3_2_Compatibility>
+#include <QOpenGLFunctions_3_2_Core>
+
+#include <glm/glm.hpp>
+#include <glm/vector_relational.hpp>
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <memory>
+#include <vector>
+
+class DrawableGrid {
+public:
+    DrawableGrid(): gl(nullptr){};
+    virtual ~DrawableGrid() = default;
+
+    void initializeGL(ShaderCompiler::GLFunctions* functions);
+
+    void draw(GLfloat *proj_mat, GLfloat *view_mat, const glm::vec4& camera, const glm::vec3& planePosition);
+
+    glm::vec4 color;
+    glm::vec3 lightPosition;
+
+
+protected:
+
+    ShaderCompiler::GLFunctions* gl;
+
+    GLuint program;
+
+	GLuint vao;
+	GLuint vbo_vertices;
+	GLuint vbo_normals;
+	GLuint vbo_indices;
+};
+
+#endif	  // VISUALISATION_MESHES_DRAWABLE_MESH_HPP_
